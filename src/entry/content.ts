2044461d3671c10511e00f42a58aabd927d38686
ArtemZip <abosnyak@ya.ru>
2024-01-20 17:57:15 +0100

    full encapsulation

--- src/entry/content.ts
+++ src/entry/content.ts
@@ -4,7 +4,16 @@ import App from '../view/popup.vue'
 const modal = document.createElement("div");
 modal.id = 'example-modal'
 
-document.body.append(modal);
+const shadow = document.createElement('div');
+const shadowRoot = shadow.attachShadow({ mode: 'open' })
+
+const styles = document.createElement('link');
+styles.rel = "stylesheet";
+styles.href = chrome.runtime.getURL("css/content.css");
+
+shadowRoot.appendChild(modal);
+shadowRoot.appendChild(styles);
+document.body.append(shadow);
 
 createApp(App).mount(modal);
 
