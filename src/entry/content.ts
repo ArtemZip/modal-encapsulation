7ecf217b4d82e12977eca5f2c41ea1d0fa82983d
ArtemZip <abosnyak@ya.ru>
2024-01-21 16:09:53 +0100

    remove redundant code

--- src/entry/content.ts
+++ src/entry/content.ts
@@ -5,15 +5,10 @@ import App from '../view/popup.vue'
 const shadow = document.createElement('div');
 const shadowRoot = shadow.attachShadow({ mode: 'open' })
 
-const styles = shadowRoot.ownerDocument.createElement('link');
-styles.rel = "stylesheet";
-styles.href = chrome.runtime.getURL("css/content.css");
-
 const modal = shadowRoot.ownerDocument.createElement("div");
 modal.id = 'example-modal'
 
 shadowRoot.appendChild(modal);
-shadowRoot.appendChild(styles);
 
 shadow.style.position = "absolute";
 shadow.style.zIndex = "9999999";
